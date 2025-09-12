31ff218a4390d938ed03842afafdbaa0ad1e180b
amplil <aikotobahaaburakatabura@gmail.com>
2025-09-12 15:45:38 +0900

    Update project structure: rename apps and add TypeScript settings. Changed app names from 'docs' to 'web2' and 'web' to 'web1' in package.json and page.tsx files. Added .vscode/settings.json for TypeScript configuration.

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="docs" className={styles.secondary}>
+        <Button appName="web2" className={styles.secondary}>
           Open alert
         </Button>
       </main>
